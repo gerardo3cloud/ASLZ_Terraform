059457bd48b476d90f4c6cad2de2eaddda6ee901
gerardo3cloud <gerardo.rodriguez@3cloudsolutions.com>
2025-02-21 15:23:00 -0600

    update backend.tf

--- Project1/backend.tf
+++ Project1/backend.tf
@@ -4,9 +4,9 @@ terraform {
     storage_account_name = "satfstatefileproject1"                # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
     container_name       = "tfstatefile"                          # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
     key                  = "terraform.tfstate"               # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
-    client_id            = "00000000-0000-0000-0000-000000000000" # Can also be set via `ARM_CLIENT_ID` environment variable.
-    client_secret        = "************************************" # Can also be set via `ARM_CLIENT_SECRET` environment variable.
-    subscription_id      = "00000000-0000-0000-0000-000000000000" # Can also be set via `ARM_SUBSCRIPTION_ID` environment variable.
-    tenant_id            = "00000000-0000-0000-0000-000000000000" # Can also be set via `ARM_TENANT_ID` environment variable.
+    #client_id            = "00000000-0000-0000-0000-000000000000" # Can also be set via `ARM_CLIENT_ID` environment variable.
+    #client_secret        = "************************************" # Can also be set via `ARM_CLIENT_SECRET` environment variable.
+    #subscription_id      = "00000000-0000-0000-0000-000000000000" # Can also be set via `ARM_SUBSCRIPTION_ID` environment variable.
+    #tenant_id            = "00000000-0000-0000-0000-000000000000" # Can also be set via `ARM_TENANT_ID` environment variable.
   }
 }
